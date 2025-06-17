03a810ef9d2c84744724550cac86097ab272d17f
Kevin Ardhana <kevinardhana096@gmail.com>
2025-06-17 12:08:35 +0800

    feat : layout edit profile and profile settings

--- app/src/main/java/com/example/spendly/fragment/HomeFragment.java
+++ app/src/main/java/com/example/spendly/fragment/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.example.spendly;
+package com.example.spendly.fragment;
 
 import android.os.Bundle;
 
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.spendly.R;
 
 public class HomeFragment extends Fragment {
 
